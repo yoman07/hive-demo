592273e31b246d318be8cc684165f9c0a7fae305
rbarzyczak <roman.barzyczak@brainly.com>
2014-04-03 18:42:44 +0200

    add configuration to bootstrap.sh

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -5,3 +5,7 @@ sudo dpkg -i erlang-solutions_1.0_all.deb
 
 sudo apt-get update
 sudo apt-get -y install erlang
+sudo apt-get -y install git
+
+git clone https://github.com/brainly/hive.git /home/vagrant/hive
+sudo apt-get -y install supervisor
