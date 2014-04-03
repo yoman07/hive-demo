58e8a5cfb48dac4fab7a855214aca89c5a5de092
rbarzyczak <roman.barzyczak@brainly.com>
2014-04-03 18:40:06 +0200

    Add vagrant configuration

---
+++ provision/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+sudo dpkg -i erlang-solutions_1.0_all.deb
+
+sudo apt-get update
+sudo apt-get -y install erlang
