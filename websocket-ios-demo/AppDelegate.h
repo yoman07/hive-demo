01d7d14ba67c3c46313132ad035055131ced15a7
rbarzyczak <roman.barzyczak@brainly.com>
2014-04-08 11:29:58 +0200

    new version of ios application

---
+++ websocket-ios-demo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  websocket-ios-demo
+//
+//  Created by Roman Barzyczak on 03.04.2014.
+//  Copyright (c) 2014 Roman Barzyczak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
